2888bd870ebdb3df8ae98f650883a01fd97b75ff
palaniyokesh <palaniyokesh8@gmail.com>
2026-05-16 21:30:36 +0530

    Add the mongo urls

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ app.config.update(
 )
 
 # Production Safe Database Config
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost:27017/CampusBitesDB")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://127.0.0.1:5000/CampusBitesDB")
 mongo = PyMongo(app)
 
 # 3. THE ROUTES
